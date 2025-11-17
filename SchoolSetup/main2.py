62231ca167b37c8324d9a2c74f53ce91fa8052a4
Ice424 <e4elliotd@gmail.com>
2025-11-17 09:28:48 +0000

    Updates

--- SchoolSetup/main2.py
+++ SchoolSetup/main2.py
@@ -3,7 +3,7 @@ import os
 
 from textual import work
 from textual.app import App, ComposeResult
-from textual.containers import HorizontalGroup, VerticalGroup
+from textual.containers import HorizontalGroup, VerticalGroup, VerticalScroll
 from textual.widgets import Button, Digits, Footer, Header, ProgressBar, Checkbox, Input, Label
 from textual.screen import Screen
 from textual.worker import Worker, get_current_worker
@@ -13,6 +13,9 @@ PATH = "SchoolSetup"
 GIT_PATH = os.path.abspath("~\\AppData\\Local\\Programs\\Git\\cmd\\git.exe")
 
 CODE_PATH = os.path.abspath("~\\AppData\\Local\\Programs\\Microsoft VS Code\\code.exe")
+
+global download_count
+download_count = 0
 
 def get_stuff(username: str):
     r = requests.get(f"https://api.github.com/users/{username}/repos")
@@ -29,6 +32,7 @@ def get_stuff(username: str):
     
 class UsernameInput(Screen):
     def compose(self) -> ComposeResult:
+        yield Header()
         yield Label("Enter your Github Username:")
         yield Input(placeholder="20edunn", id="user_input")
         
@@ -36,22 +40,25 @@ class UsernameInput(Screen):
         bar1.update(total=1, progress=1)
         bar2 = ProgressBar(id="Bar2")
         bar2.update(total=1, progress=1)
+        bar3 = ProgressBar(id="Bar3")
         
         yield bar1
         yield bar2  
+        yield bar3
+        yield Footer()
     def on_input_submitted(self, event: Input.Submitted) -> None:
         username = event.value.strip()
         self.app.push_screen(EmailCheck(username))
-
 
 class EmailCheck(Screen):
     def __init__(self, username: str, name: str | None = None, id: str | None = None, classes: str | None = None) -> None:
         super().__init__(name, id, classes)
         self.username = username
+        self.email = ""
         try:
             self.email, self.repos = get_stuff(self.username)
         except Exception:
-            self.app.switch_screen(EmailInput())
+            self.app.push_screen(EmailInput())
         
     def compose(self) -> ComposeResult:
         yield Header()
@@ -67,9 +74,11 @@ class EmailCheck(Screen):
         bar1.update(total=1, progress=1)
         bar2 = ProgressBar(id="Bar2")
         bar2.update(total=1, progress=1)
+        bar3 = ProgressBar(id="Bar3")
         
         yield bar1
         yield bar2  
+        yield bar3
        
         yield Footer()
     def on_button_pressed(self, event: Button.Pressed) -> None:
@@ -88,9 +97,11 @@ class EmailInput(Screen):
         bar1.update(total=1, progress=1)
         bar2 = ProgressBar(id="Bar2")
         bar2.update(total=1, progress=1)
+        bar3 = ProgressBar(id="Bar3")
         
         yield bar1
         yield bar2  
+        yield bar3
         yield Footer()
     def on_input_submitted(self, event: Input.Submitted) -> None:
         username = event.value.strip()
@@ -98,7 +109,33 @@ class EmailInput(Screen):
 
 class GitTime(Screen):
     pass
+
+class OtherApps(Screen):
+    def __init__(self, app:App, name: str | None = None, id: str | None = None, classes: str | None = None) -> None:
+        super().__init__(name, id, classes)
+        self.app = app
     
+    def compose(self) -> ComposeResult:
+        r = requests.get("url")
+        yield Header()
+        yield Label("What Other apps would you like")
+        scroll = VerticalScroll()
+        
+        VerticalScroll(
+            for 
+        )
+        
+        bar1 = ProgressBar(id="Bar1")
+        bar1.update(total=1, progress=1)
+        bar2 = ProgressBar(id="Bar2")
+        bar2.update(total=1, progress=1)
+        bar3 = ProgressBar(id="Bar3")
+        
+        yield bar1
+        yield bar2  
+        yield bar3
+        yield Footer()
+        
 
 class SchoolSetup(App):
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
@@ -112,27 +149,34 @@ class SchoolSetup(App):
         bar1.update(total=1, progress=1)
         bar2 = ProgressBar(id="Bar2")
         bar2.update(total=1, progress=1)
+        bar3 = ProgressBar(id="Bar3")
         
         yield bar1
         yield bar2  
+        yield bar3
     def on_mount(self) -> None:
         self.downloaded_items =0
         if not os.path.isfile(CODE_PATH):
-            self.Download("code.exe", "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user", "#Bar1", self.app)
+             self.code_worker = self.Download("code.exe", "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user", self.app)
         if not os.path.isfile(GIT_PATH):
             url = self.fetch_latest_release("https://api.github.com/repos/git-for-windows/git/releases/latest")
-            self.Download("git.exe", url, "#Bar2", self.app)
-        self.push_screen(UsernameInput())
+            self.git_worker = self.Download("git.exe", url, self.app)
+        self.push_screen(OtherApps(self))
         
 
     def fetch_latest_release(self, url) -> str:
         r = requests.get(url)
-        DLurl = r.json()["assets"][0]["browser_download_url"]
-        return DLurl
+        for asset in r.json()["assets"]:
+            if ".exe" in asset["browser_download_url"]:
+                return asset["browser_download_url"]
+        return url
     
     @work(exclusive=False, thread=True)
-    def Download(self, name:str, url:str, progress_bar_name:str, app:App) -> None:
+    def Download(self, name:str, url:str, app:App) -> None:
         PATH = "SchoolSetup"
+        global download_count
+        download_count += 1
+        progress_bar_name = "#Bar" + str(download_count%3)
         progress_bar = app.query_one(progress_bar_name, ProgressBar)
         worker = get_current_worker()
         response = requests.get(url, stream=True)
--- SchoolSetup/test.py
+++ SchoolSetup/test.py
@@ -1,37 +1,16 @@
 import requests
-import os
-
-from textual import work
-from textual.app import App, ComposeResult
-from textual.containers import VerticalScroll
-from textual.widgets import Button, Static, ProgressBar
-from textual.screen import Screen
-from textual.worker import Worker, get_current_worker
-
-class AnotherScreen(Screen):
-    def __init__(self, main, name: str | None = None, id: str | None = None, classes: str | None = None) -> None:
-        super().__init__(name, id, classes)
-        self.main = main
-        
-    def compose(self) -> ComposeResult:
-        yield Static("1 Download Done")
-        with VerticalScroll(id="weather-container"):
-            yield ProgressBar(id="Bar1")
-            yield ProgressBar(id="Bar2")
-
-class DownloadApp(App):
-    def compose(self) -> ComposeResult:
-        self.downloads_complete = 0
-        yield Button()
-        with VerticalScroll(id="weather-container"):
-            yield ProgressBar(id="Bar1")
-            yield ProgressBar(id="Bar2")
-
-    def on_button_pressed(self, event: Button.Pressed) -> None:
-        
-        
 
 
-if __name__ == "__main__":
-    app = DownloadApp()
-    app.run()
+def fetch_latest_release(url) -> str:
+    r = requests.get(url)
+    for asset in r.json()["assets"]:
+        if ".exe" in asset["browser_download_url"]:
+            return asset["browser_download_url"]
+    return url
+
+
+url = fetch_latest_release(
+    "https://api.github.com/repos/git-for-windows/git/releases/latest"
+)
+
+print(f"\n{url}")
